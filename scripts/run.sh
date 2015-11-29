bebc9900e79a3bcc4cbbb39fc1ad3ac55c9ef577
haruosuz <haruosuz@yahoo.co.jp>
2015-11-29 21:43:56 +0900

    update README.md scripts/

---
+++ scripts/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -euo pipefail
+
+# Creating directories
+
+mkdir data analysis
+
+# Creating some empty README files in data/
+
+touch data/README
+
+# Redirecting standard out to a file
+
+ls -l data/README > analysis/output.txt
